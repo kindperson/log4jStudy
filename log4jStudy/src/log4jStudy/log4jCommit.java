09c6859bf7dc215dde418d831b66a1fa30bc39c8
Junhu Tian <18042319280@qq.com>
2017-05-03 09:42:59 +0800

    第二次提交

--- log4jStudy/src/log4jStudy/log4jCommit.java
+++ log4jStudy/src/log4jStudy/log4jCommit.java
@@ -3,5 +3,6 @@ package log4jStudy;
 public class log4jCommit {
 	public static void main(String[] args) {
 		System.out.println("我会用myeclipse提交代码到GitHub了！");
+		System.out.println("i can use myeclipse commit to github!");
 	}
 }
