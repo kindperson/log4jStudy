446dd1d763f8fa241c5775edd9825d8ed83dfb27
Junhu Tian <18042319280@qq.com>
2017-05-03 09:57:35 +0800

    删除项目提交的测试代码

--- log4jStudy/src/log4jStudy/log4jCommit.java
+++
@@ -1,9 +0,0 @@
-package log4jStudy;
-
-public class log4jCommit {
-	public static void main(String[] args) {
-		System.out.println("我会用myeclipse提交代码到GitHub了！");
-		System.out.println("i can use myeclipse commit to github!");
-		System.err.println("i canot do it");
-	}
-}
